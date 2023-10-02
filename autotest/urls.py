54fc74cfcfe7c46f030726895b481a0e764b51ea
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2023-10-02 15:01:17 +0200

    favicon?

--- autotest/urls.py
+++ autotest/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from . import views
 
@@ -24,4 +26,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("sales/", include("sales.urls")),
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.png'))),
 ]
