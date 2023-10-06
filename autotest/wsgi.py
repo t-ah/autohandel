9c825df85fbf0ab22a747810f91e14f56477b846
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2023-10-06 10:03:31 +0200

    test auto migration

--- autotest/wsgi.py
+++ autotest/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autotest.settings')
 
 application = get_wsgi_application()
+call_command("migrate")
